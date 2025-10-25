877c53e69edcf6b536fffbe980b4beb200779a36
Aashvi30 <maniyatekade@gmail.com>
2025-10-25 11:32:11 +0530

    vercel.json added

--- foodify-backend/index.js
+++ foodify-backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => console.log("DB Connected"))
+}).then(() => console.log("DB Connected successfully"))
   .catch(err => console.log(err));
 
 app.use("/api/auth", authRoutes);
